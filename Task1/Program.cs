9f8d7f6d29d9a8d1e2f34101de176791b41d5838
Ivan <mmavrin80@gmail.com>
2022-04-18 13:52:14 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿int Parse(string message)
+{
+    Console.WriteLine(message);
+    string strNum = Console.ReadLine();
+    int Num = int.Parse(strNum);
+    return Num;
+}
+
+void Even (int arg1, int arg2)
+{
+    if (arg1 > arg2)
+    {
+        return;
+    }
+    if (arg1 % 2 == 0)
+    {
+        Console.Write(arg1 + " ");
+        Even(arg1 + 2, arg2);
+    }
+    else
+    {
+        Even(arg1 + 1, arg2);
+    }
+}
+
+int M = Parse("Введите первое число: ");
+int N = Parse("Введите второе число: ");
+Even(M,N);
