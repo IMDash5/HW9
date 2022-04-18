eb2cfcb0997546307dba3f63d772e17182ce57f3
Ivan <mmavrin80@gmail.com>
2022-04-18 14:01:54 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿int Parse(string message)
+{
+    Console.WriteLine(message);
+    string strNum = Console.ReadLine();
+    int Num = int.Parse(strNum);
+    return Num;
+}
+
+int Sum(int arg1, int arg2)
+{
+    if (arg1 > arg2)
+    {
+        return 0;
+    }
+    return arg1 + Sum(arg1 + 1, arg2);
+}
+
+int M = Parse("Введите первое число: ");
+int N = Parse("Введите второе число: ");
+int a = Sum(M,N);
+Console.WriteLine(a);
